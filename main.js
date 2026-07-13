ba6ef1232bd384ef74aae71d11c9c358028bd3e1
cochanae-del <co.chanae@gmail.com>
2026-07-13 05:53:31 +0000

    Correct syntax error.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 //var num = Number(prompt("Enter a number"));
 
 let num = 9
-if (num===10){console.log(true)}
-else{console.log(false)};
+if (num===10)  {
+    console.log(true);
+} else  {
+    console.log(false);
+}
 
 console.log(num===10);
